4048a6b9e4c9130530e25a7e7dbf32c58e6c61c9
Martin Dumas <dumasmartin9@gmail.com>
2023-11-05 22:07:53 +0200

    1

--- Cinema Room Manager/src/Main.java
+++ Cinema Room Manager/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Cinema:");
@@ -9,5 +11,24 @@ public class Main {
             }
             System.out.println();
         }
+        Scanner src = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int rows = src.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int seats = src.nextInt();
+        int total = rows*seats;
+        int frontHalf, backHalf;
+        System.out.println("Total income:");
+        if (total > 60){
+            if (total % 2 == 0){
+                frontHalf = rows / 2;
+                backHalf = rows / 2;
+            } else {
+                frontHalf = rows / 2;
+                backHalf = rows / 2 + 1;
+            }
+            System.out.println("$" + (frontHalf * seats * 10 + (backHalf * seats * 8)));
+        }
+        System.out.println("$" + total * 10);
     }
 }
